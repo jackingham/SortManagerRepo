469b265eaab279dae478fc26dc243e729686b4c4
jackingham <jackingham1998@gmail.com>
2021-04-01 12:05:14 +0100

    Added basic functional tests for merge and bubble sort

--- src/test/java/com/sparta/jack/FunctionalTests.java
+++ src/test/java/com/sparta/jack/FunctionalTests.java
@@ -1,11 +1,14 @@
 package com.sparta.jack;
 
+import com.sparta.jack.exceptions.ArrayTooSmallException;
 import com.sparta.jack.interfaces.Sorter;
 import com.sparta.jack.sort.SortFactory;
 import com.sparta.jack.utilities.SortTypes;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class FunctionalTests {
     static Sorter bubbleSorter;
@@ -20,6 +23,16 @@ public class FunctionalTests {
 
     @Test
     @DisplayName(" Tests function of bubble sort")
-    public void testBubbleSort(){};
+    public void testBubbleSort() throws NullPointerException, ArrayTooSmallException {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, bubbleSorter.sortArray(new int[]{5, 9, 2, 1, 8, 4, 3, 7, 6}));
+    }
 
+
+    @Test
+    @DisplayName(" Tests function of merge sort")
+    public void testMergeSort() throws NullPointerException, ArrayTooSmallException {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, mergeSorter.sortArray(new int[]{5, 9, 2, 1, 8, 4, 3, 7, 6}));
+    }
 }
+
+
